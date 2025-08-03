129fa4dcf0f576f3d0c9e231d17e2a39ae168cce
MatinT-SA <matin.taherzadeh.mmtsa@gmail.com>
2025-08-04 00:52:03 +0330

    dashboard content

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,3 +1,36 @@
+import styled from "@emotion/styled";
+
+const Container = styled.div`
+  background-color: rgba(255, 255, 255, 0.4);
+  padding: 2rem;
+  border-radius: 7px;
+  color: #000433;
+  max-width: 1288px;
+  margin: 3rem 2rem;
+  height: auto;
+`;
+
+const Heading = styled.h1`
+  font-size: 25px;
+  font-weight: 700;
+  margin-bottom: 1rem;
+`;
+
+const Message = styled.p`
+  font-size: 20px;
+  font-weight: 600;
+  line-height: 40px;
+  white-space: pre-line;
+`;
+
 export default function Dashboard() {
-  return <div>Dashboard</div>;
+  return (
+    <Container>
+      <Heading>Hello, Parnia 👋🏻</Heading>
+      <Message>
+        Im very happy you are here,
+        <br /> I hope you find this dashboard easy and useful to use ☺️
+      </Message>
+    </Container>
+  );
 }
