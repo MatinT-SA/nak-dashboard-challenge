cbf02fa3596d5caf93bfa098496c30028802cf1d
MatinT-SA <matin.taherzadeh.mmtsa@gmail.com>
2025-08-03 11:08:09 +0330

    setting up our styles using EmotionJS entirely

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,45 @@
-import "./App.css";
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import { useAuthStore } from "./store/authStore";
+/** @jsxImportSource @emotion/react */
+import { Global } from "@emotion/react";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import globalStyles from "./styles/global";
+
+import Attributes from "./pages/Attributes";
+import Dashboard from "./pages/Dashboard";
+import Products from "./pages/Products";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
-import Dashboard from "./pages/Dashboard";
-import Attributes from "./pages/Attributes";
-import Products from "./pages/Products";
+import { useAuthStore } from "./store/authStore";
 
 function App() {
   const isLoggedIn = useAuthStore((state) => state.isLoggedIn);
 
   return (
-    <BrowserRouter>
-      <Routes>
-        {/* Public routes */}
-        {!isLoggedIn && (
-          <>
-            <Route path="/signin" element={<SignIn />} />
-            <Route path="/signup" element={<SignUp />} />
-            <Route path="*" element={<Navigate to="/signin" />} />
-          </>
-        )}
+    <>
+      <Global styles={globalStyles} />
 
-        {/* Private routes */}
-        {isLoggedIn && (
-          <>
-            <Route path="/" element={<Dashboard />} />
-            <Route path="/attributes" element={<Attributes />} />
-            <Route path="/products" element={<Products />} />
-            <Route path="*" element={<Navigate to="/" />} />
-          </>
-        )}
-      </Routes>
-    </BrowserRouter>
+      <BrowserRouter>
+        <Routes>
+          {/* Public routes */}
+          {!isLoggedIn && (
+            <>
+              <Route path="/signin" element={<SignIn />} />
+              <Route path="/signup" element={<SignUp />} />
+              <Route path="*" element={<Navigate to="/signin" />} />
+            </>
+          )}
+
+          {/* Private routes */}
+          {isLoggedIn && (
+            <>
+              <Route path="/" element={<Dashboard />} />
+              <Route path="/attributes" element={<Attributes />} />
+              <Route path="/products" element={<Products />} />
+              <Route path="*" element={<Navigate to="/" />} />
+            </>
+          )}
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
